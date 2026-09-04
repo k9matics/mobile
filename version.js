e2c13090714efdb415d40c052dbc05a399ddf434
k9matics <doggo.planto@gmail.com>
2026-09-04 23:02:56 +0200

    Initialize version.js with app metadata
    Added version information for CANINE GAIT HUD.

---
+++ version.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/*
+  CANINE GAIT HUD
+  Projektversion: v2.3.1
+  Versionsdatum: 04.09.2026
+  Datei: version.js
+
+  Nur hier Version und Datum ändern.
+*/
+
+window.APP_META = {
+  name: "CANINE GAIT HUD",
+  version: "2.3.1",
+  date: "04.09.2026"
+};
