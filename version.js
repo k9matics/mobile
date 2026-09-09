11941f23e18a1454d8f1daec0905533b8c3d67b2
k9matics <doggo.planto@gmail.com>
2026-09-09 13:24:28 +0200

    Update version and metadata for HARNELYZER BEST FIT
    Updated application metadata with new version and details.

--- version.js
+++ version.js
@@ -1,8 +1,18 @@
-"use strict";
+""use strict";
+
+/*
+  HARNELYZER BEST FIT
+  Zentrale Projektversion
+  Nur hier Version und Datum ändern.
+*/
 
 window.APP_META = {
-  name: "HARNELYZER",
-  version: "0.4.0",
-  releaseDate: "2026-09-06",
-  channel: "radar-3d-kombi"
+  name: "HARNELYZER BEST FIT",
+  shortName: "HARNELYZER",
+  tagline: "Geschirrpassform in Bewegung verstehen",
+  sensorName: "HARNELYZER SENSOR",
+  version: "3.0.0",
+  date: "09.09.2026",
+  appUrl: "https://k9matics.github.io/mobile/",
+  bestFitUrl: "https://k9matics.github.io/-Best-fit/"
 };
